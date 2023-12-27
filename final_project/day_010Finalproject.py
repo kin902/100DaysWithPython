4727c63e22332f75a63fa3586d2a2bda7b42e8f7
Vinh Vo <vohuynhvinh@gmail.com>
2023-12-27 11:38:11 +0700

    Refactor exercise day 010 use dictionary instead of list

--- final_project/day_010Finalproject.py
+++ final_project/day_010Finalproject.py
@@ -1,10 +1,15 @@
 # 1 Create all variable need
 # To check if the user use the old number to continue calculate
 next_time = False
-next_time_var = int(0)
 # A list that contain all number need to calculate
-# Here is the index and meaning: 0 - number1, 1 - number2, 2 - answer, 3 - pre answer
-math_list = list([0, 0, 0, 0])
+FIRST_NUMBER = "first_number"
+SECOND_NUMBER = "second_number"
+RESULT_NUMBER = "result_number"
+math_dict = {
+    FIRST_NUMBER: 0,
+    SECOND_NUMBER: 0,
+    RESULT_NUMBER: 0,
+}
 # Symbol that need to calculate
 symbol = str()
 # To check if the user want to calculate with the pre answer or and or create a new calculate
@@ -51,43 +56,38 @@ def operator():
 
 # 3 code need to calculate
 while continue_end_start_new == "y" or continue_end_start_new == "n":
-
-    # Check if this a new one or a odd one
-    # If the odd one we don't need the first number
+    # First number
     if not next_time:
-        math_list[0] = float(input("What's your first number? :"))
-    elif next_time:
-        math_list[3] = math_list[2]
-        next_time_var = 3
-    print()
+        math_dict[FIRST_NUMBER] = float(input("What's your first number? :"))
 
+    # Operator
     symbol = operator()
     print()
 
-    # Ask the user to insert second number
-    math_list[1] = float(input(" What's the next number? :"))
+    # Second number
+    math_dict[SECOND_NUMBER] = float(input(" What's the next number? :"))
 
     # Calculate the two number and return to the user
     if symbol == "+":
-        math_list[2] = math_list[next_time_var] + math_list[1]
+        math_dict[RESULT_NUMBER] = math_dict[FIRST_NUMBER] + math_dict[SECOND_NUMBER]
     elif symbol == "-":
-        math_list[2] = math_list[next_time_var] - math_list[1]
+        math_dict[RESULT_NUMBER] = math_dict[FIRST_NUMBER] - math_dict[SECOND_NUMBER]
     elif symbol == "*":
-        math_list[2] = math_list[next_time_var] * math_list[1]
+        math_dict[RESULT_NUMBER] = math_dict[FIRST_NUMBER] * math_dict[SECOND_NUMBER]
     elif symbol == "/":
-        math_list[2] = math_list[next_time_var] / math_list[1]
+        math_dict[RESULT_NUMBER] = math_dict[FIRST_NUMBER] / math_dict[SECOND_NUMBER]
 
     # After calculate print the final formular
-    print(f"{math_list[next_time_var]} {symbol} {math_list[1]} = {math_list[2]}")
+    print(f"{math_dict[FIRST_NUMBER]} {symbol} {math_dict[SECOND_NUMBER]} = {math_dict['result_number']}")
 
     # Check do the user want to continue or end or start a new calculate
-    continue_end_start_new = input(f"Type 'y' to continue calculating with {math_list[2]}, or type 'n' to start new "
-                                   f"calculation, or type e to end the calculate: ")
+    continue_end_start_new = input(f"Type 'y' to continue calculating with {math_dict['result_number']}, or type 'n' "
+                                   f"to start new calculation, or type e to end the calculate: ")
 
     # Check if the user continue to calculate with the old number?
     if continue_end_start_new == "y":
+        math_dict[FIRST_NUMBER] = math_dict[RESULT_NUMBER]
         next_time = True
     # Else then just reset everything to normal
     else:
         next_time = False
-        next_time_var = 0
