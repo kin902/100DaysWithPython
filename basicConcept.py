84f070ec2e99467b6d4752c3b99c01437bab4389
Kin Loki <kinquay@gmail.com>
2024-01-05 19:52:24 +0700

    update some more code - with mistake remove

--- basicConcept.py
+++ basicConcept.py
@@ -1,9 +1,9 @@
-'''
+"""
 #Data type
 #-String
 print("")
 print("String")
-print("E.g 123" +"345")
+print("E.g. 123" +"345")
 
 #-Integer
 
@@ -22,27 +22,27 @@ print("E.g 3.14159")
 
 print("")
 print("Boolean")
-print("E.g True / False")
+print("E.g. True / False")
 
 #The order in a String
 print("")
 print("the order in a String")
 print("H e l l o")
 print("0 1 2 3 4")
-print("E.g Hello[0] =" , "Hello"[0])
+print("E.g. Hello[0] =" , "Hello"[0])
 
-#To conver a str, float, int into any data type
+#To convert a str, float, int into any data type
 print("")
-print("To conver a str, float, int into any data type")
-print("3 way to conver a str, float, ine into any data type")
-print("Way1 use str(var) to conver to str")
-print("Way2 use float(var) to conver to float")
-print("Way3 use int(var) to conver to int")
+print("To convert a str, float, int into any data type")
+print("3 way to convert a str, float, ine into any data type")
+print("Way1 use str(var) to convert to str")
+print("Way2 use float(var) to convert to float")
+print("Way3 use int(var) to convert to int")
 
 #Mathemathical operation
 
 print("")
-#print("Mathemathical operation")
+#print("Mathematical operation")
 2 + 4
 5 - 3
 6 / 3
@@ -57,12 +57,6 @@ print("")
 # /
 # +
 # -
-
-# PEMDASLR
-# ()
-# **
-# * /
-# + -
 
 print("Rounding number")
 print("Use func round(num/operation , d.p)")
@@ -79,4 +73,4 @@ print("print(f\"Your score is {score}, your height is {height}, you are winning 
 print("")
 print("output")
 print("Your score is 1, your height is 1.8, you are winning is True")
-'''
+"""
