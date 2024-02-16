873e136067d32d8298d64cc949d20d603480b8f5
Kin Loki <kinquay@gmail.com>
2024-02-16 09:55:42 +0700

    upload some more code

--- day_016.py
+++ day_016.py
@@ -5,8 +5,12 @@ print(my_screen.canvheight)
 my_screen.exitonclick()
 """
 import prettytable
-
+# Create an object
 table = prettytable.PrettyTable()
+# Create first column
 table.add_column("Pokemon name", ["Pikachu", "Charmander", "Squirtle"])
+# Create the second column
 table.add_column("Type", ["Electric", "Water", "Fire"])
 print(table)
+table.clear()
+print()
