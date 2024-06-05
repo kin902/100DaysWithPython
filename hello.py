9153908f4756f228cd6d850180d58fa5158d233e
quaykin <kinquay@gmail.com>
2024-06-05 10:02:23 +0700

    upload_some_more_code

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
-with open("final_project/day_024Finalproject/Input/Letters/starting_letter.txt") as letter:
-    module_letter = str(letter.read())
-    module_letter = module_letter.replace("[name]", "hello")
-    print(module_letter)
+from tkinter import *
+
+root = Tk()
+
+button = Button(root, text="Click me!")
+# make sure to add "/" not "\"
+button.pack()
+# Displaying the button
+
+root.mainloop()
