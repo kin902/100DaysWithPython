e322feebe707522c9b3ad781a76ef643b83230cc
quaykin <kinquay@gmail.com>
2024-06-17 21:07:12 +0700

    update_more_code

---
+++ day_030/day_030.py
@@ -0,0 +1,35 @@
+# Exercise 30
+"""
+fruits = eval(input())
+
+
+# 🚨 Do not change the code above
+
+# TODO: Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
+
+
+#  🚨 Do not change the code below
+make_pie(4)
+"""
+
+# Exercise 34
+# eval() function will create a list of dictionaries using the input
+facebook_posts = eval(input())
+
+total_likes = 0
+# TODO: Catch the KeyError exception
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        total_likes = total_likes + 0
+
+
+print(total_likes)
